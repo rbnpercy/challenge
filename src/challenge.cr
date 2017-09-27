0ac71cd00f9efbef10269363bf67f9a01eb5c739
Robin Percy <robin@percy.pw>
2017-09-27 20:34:00 +0100

    JWT & LocalStorage working correctly now!

--- src/challenge.cr
+++ src/challenge.cr
@@ -1,16 +1,30 @@
 require "./challenge/*"
 require "kemal"
+require "kemal-session"
 require "./auth"
 
 module Challenge
 
-  get "/" do
-    render "src/challenge/views/home.ecr", "src/challenge/views/layouts/main.ecr"
+  Kemal::Session.config do |config|
+    config.cookie_name = "sess_id"
+    config.secret = "3c74e7e506f9cfdb23cc22247d37bc21a9f04d296140f3cc0193dbb94776a3dec42fb0f3a1056ae8c0df346d94f34d6b9f48d1465a05b43d4bee0b616738bdf5"
+    config.gc_interval = 1.minutes # 2 minutes
   end
 
-  get "/:name" do |env|
-    name = env.params.url["name"]
-    render "src/challenge/views/hello.ecr", "src/challenge/views/layouts/main.ecr"
+  class UserStorableObject
+    JSON.mapping({
+      id_token: String
+    })
+
+    include Kemal::Session::StorableObject
+
+    def initialize(@id_token : String); end
+  end
+
+
+######  BEGIN ROUTES FOR APP ######
+  get "/" do
+    render "src/challenge/views/home.ecr", "src/challenge/views/layouts/main.ecr"
   end
 
   get "/auth/login" do
@@ -22,17 +36,30 @@ module Challenge
     jwt = Auth.get_auth_token(code)
     env.response.headers["Authorization"] = "Bearer #{jwt}"  # Set the Auth header with JWT.
 
-    user = env.response.headers.inspect
+    user = UserStorableObject.new(jwt)
+    env.session.object("user", user)
 
-    # env.redirect "/success"
-
-    render "src/challenge/views/callback.ecr", "src/challenge/views/layouts/main.ecr"
+    env.redirect "/success"
+    # render "src/challenge/views/callback.ecr", "src/challenge/views/layouts/main.ecr"
   end
 
   get "/success" do |env|
+    user = env.session.object("user").as(UserStorableObject)
+    env.response.headers["Authorization"] = "Bearer #{user.id_token}"
+
     heads = env.response.headers.inspect
+
     render "src/challenge/views/success.ecr", "src/challenge/views/layouts/main.ecr"
   end
+
+  get "/auth/logout" do |env|
+    env.session.destroy
+
+    heads = env.response.headers.inspect
+
+    render "src/challenge/views/logout.ecr", "src/challenge/views/layouts/main.ecr"
+  end
+######  END OF ROUTES  ######
 
 
   Kemal.config.port = 6969
